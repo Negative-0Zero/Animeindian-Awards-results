c9dd408ca6a40d4917e20726b3977da5c70fb540
AnimeIndian-Awards <negativezero338@gmail.com>
2026-02-18 11:54:56 +0000

    my push

--- anime-awards/utils/supabase/client.ts
+++ anime-awards/utils/supabase/client.ts
@@ -1,15 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-        auth: {
-            persistSession: true,
-            storageKey: 'animeindian-auth',
-            storage: typeof window !== 'undefined' ? localStorage : undefined,
-            autoRefreshToken: true,
-            detectSessionInUrl: true
-        }
-    }
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
